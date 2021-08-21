bc47ac3e0eaca5ffa3de10cbb6d0e74d77b6771b
Subhamdas20 <subham21.sd@gmail.com>
2021-08-22 00:30:23 +0530

    data stores in dictionary

--- computation.sh
+++ computation.sh
@@ -5,17 +5,22 @@ echo "Enter 2nd integer"
 read b;
 echo "Enter 3rd integer"
 read c;
+declare -A computation
 
 expr1=$((a+b*c))
 #echo $expr1
 expr2=$((a*b+c))
-<<<<<<< HEAD
-echo $expr2
-=======
+
+# echo $expr2
+
 #echo $expr2
 expr3=$((c+a/b))
 #echo $expr3
-
->>>>>>> uc4
 expr4=$((a%b+c))
 
+computation[ex1]=$expr1
+computation[ex2]=$expr2
+computation[ex3]=$expr3
+computation[ex4]=$expr4
+
+# echo ${computation[@]}
